5d730a3b99347c0cf590d2a7dfc870beb93fac30
ozborne132 <ozborne132@gmail.com>
2017-12-13 19:29:32 -0800

    Finish google auth

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+// Load User Model
+require('./models/User');
 
 // Passport Config
 require('./config/passport')(passport);
@@ -8,11 +13,44 @@ require('./config/passport')(passport);
 // Load Routes
 const auth = require('./routes/auth');
 
+// Load keys 
+const keys = require('./config/keys');
+
+
+// Map global promises
+mongoose.Promise = global.Promise;
+
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, {
+  useMongoClient: true
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('It works!');
 });
+
+// Setup cookie and session middleware
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+
+}))
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+  res.locals.use = req.user || null;
+  next();
+})
 
 // Use Routes
 app.use('/auth', auth); 
